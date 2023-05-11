8b9cc9845969a24f36f64a73a1743d1fd71eb097
Ramil Aglyamov <aglamov@gmail.com>
2023-05-12 01:22:53 +0300

    Добавлена кастомная ячейка с картинкой, кнопкой и лейблом

---
+++ ImageFeed/ImagesList/ImagesListCell.swift
@@ -0,0 +1,13 @@
+//
+//  ImagesListCell.swift
+//  ImageFeed
+//
+//  Created by Рамиль Аглямов on 10.05.2023.
+//
+
+import Foundation
+import UIKit
+
+final class ImagesListCell: UITableViewCell {
+    static let reuseIdentifier = "ImagesListCell"
+}
