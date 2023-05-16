117427d9a4c8c98e7a70f86bcbf43fbcba37efe2
Ramil Aglyamov <aglamov@gmail.com>
2023-05-17 00:29:53 +0300

    изменил размер лого и кнопки и немного поправил ее размещение по фигме

--- ImageFeed/ImagesList/ImagesListViewController.swift
+++ ImageFeed/ImagesList/ImagesListViewController.swift
@@ -22,6 +22,7 @@ class ImagesListViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         tableView.contentInset = UIEdgeInsets(top: 12, left: 0, bottom: 12, right: 0)
         
     }
