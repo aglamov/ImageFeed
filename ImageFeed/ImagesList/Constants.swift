c191222ece2b9d7183713928d27a4a7c74badda4
Ramil Aglyamov <aglamov@gmail.com>
2023-06-08 00:05:56 +0300

    Новые константы

---
+++ ImageFeed/ImagesList/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  ImageFeed
+//
+//  Created by Рамиль Аглямов on 07.06.2023.
+//
+
+import Foundation
+
+let AccessKey: String = "FsKrQNKLus_1259Z_rAjaiZ_OKoXyLvgqCqkH4FBThg"
+let SecretKey: String = "SZsDqTbV1-_zRGbbxxL6m-iCiYvaTraFIpRbTNQVEW4"
+let RedirectURI =  "urn:ietf:wg:oauth:2.0:oob"
+let AccessScope = "public+read_user+write_likes"
+let DefaultBaseURL = URL (string: "https://api.unsplash.com/")!
